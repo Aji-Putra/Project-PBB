696b8d926616afab2153b9d9e5a1ecd408eca353
ivillop <rd.akbar@yandex.com>
2025-01-14 16:01:30 +0700

    adding table participant in jury

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,17 +7,12 @@
     <title>Penilaian Baris-Berbaris</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 </head>
 <body>
-    <main>
+    <x-header></x-header>
+    <main class="mt-6 mx-10">
         {{ $slot }}
     </main>
-    <script>
-        const navLinks = document.querySelector('.nav-links')
-        function onToggleMenu(e){
-            e.name = e.name === 'menu' ? 'close' : 'menu'
-            navLinks.classList.toggle('top-[9%]')
-        }
-    </script>
 </body>
 </html>
