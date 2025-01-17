cae5ddf776cb8a171f8787627a4e6ea39ebe93af
Aji-Putra <ajiputraramadhan508@gmail.com>
2025-01-17 10:22:51 +0700

    update-auth

--- database/migrations/2025_01_13_090926_create_nilai_penaltis_table.php
+++ database/migrations/2025_01_13_090926_create_nilai_penaltis_table.php
@@ -13,18 +13,18 @@ return new class extends Migration
     {
         Schema::create('nilai_penalti', function (Blueprint $table) {
             $table->id();
-            $table->string('sekolah_id');
-            $table->integer('tidak_ikut_daftar_ulang');
-            $table->integer('tidak_ikut_upacara_pembukaan');
-            $table->integer('terlambat_ke_dp_1');
-            $table->integer('tidak_sesuai_nomor_urut');
-            $table->integer('terlewat_tampil');
-            $table->integer('kurang_lebih_personil');
-            $table->integer('anggota_sakit_di_lapangan');
-            $table->integer('merusak_properti');
-            $table->integer('melewati_garis_batas');
-            $table->integer('melebihi_waktu');
-            $table->integer('manipulasi_anggota');
+            $table->string('sekolah_id')->nullable();
+            $table->integer('tidak_ikut_daftar_ulang')->nullable();
+            $table->integer('tidak_ikut_upacara_pembukaan')->nullable();
+            $table->integer('terlambat_ke_dp_1')->nullable();
+            $table->integer('tidak_sesuai_nomor_urut')->nullable();
+            $table->integer('terlewat_tampil')->nullable();
+            $table->integer('kurang_lebih_personil')->nullable();
+            $table->integer('anggota_sakit_di_lapangan')->nullable();
+            $table->integer('merusak_properti')->nullable();
+            $table->integer('melewati_garis_batas')->nullable();
+            $table->integer('melebihi_waktu')->nullable();
+            $table->integer('manipulasi_anggota')->nullable();  
             $table->timestamps();
         });
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'), // Ganti dengan kata sandi aman
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Juri User',
+            'email' => 'juri@example.com',
+            'password' => bcrypt('password'), // Ganti dengan kata sandi aman
+            'role' => 'juri',
+        ]);
+    }
+}
