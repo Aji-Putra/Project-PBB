cc44bf6e91a0f1c0db5007158ec7c72a2c6cd636
ivillop <rd.akbar@yandex.com>
2025-01-18 11:58:52 +0700

    adding a href in rekap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -85,7 +85,9 @@
                             <p class="text-sm font-medium text-slate-300">Sudah di Nilai</p>
                             @else
                             <div class="flex gap-4 items-center" x-data="{ showModal: false }">
+                                @if ($item->status !== 'Sudah Registrasi')
                                 <a class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-green-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-500" href="/registrasi/{{ $item->id }}">Registrasi</a>
+                                @endif
                                 <button @click="showModal = true" class="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500">
                                     Nilai
                                 </button>
