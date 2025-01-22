8ba751d00eb6a82cf096cd133805797f2e730afe
Aji-Putra <ajiputraramadhan508@gmail.com>
2025-01-22 22:03:31 +0700

    -update Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,13 +16,15 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin User',
             'username' => 'admin',
+            'role' => 'Admin',
             'password' => bcrypt('password'), // Ganti dengan kata sandi aman
             'role' => 'admin',
         ]);
 
         User::create([
-            'name' => 'Juri User',
+            'name' => 'Juri PBB',
             'username' => 'juri',
+            'role' => 'PBB',
             'password' => bcrypt('password'), // Ganti dengan kata sandi aman
             'role' => 'juri',
         ]);
